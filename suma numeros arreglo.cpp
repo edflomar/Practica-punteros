424d2c3da9c8534c73fa1c6a410cfdd7558362ab
Edgar Martinez Flores <emf080996@gmail.com>
2018-08-26 12:15:14 -0500

    practica de punteros

---
+++ suma numeros arreglo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int arreglo[]={39,96,46,24,46,85,35,65,89,79};
+int *n;
+int main()
+{
+	int suma;
+	float promedio;
+	for(int i=0; i<10; i++)
+	{
+		n=&arreglo[i];
+		suma+=*n;
+		if(i==9)
+		{
+			i++;
+			promedio=suma/i;
+		}
+	}
+	cout<<"El promedio es: "<<promedio<<endl;
+	cout<<"La suma de los elementos del arreglo es: "<<suma;
+	
+	getch();
+	return 0;
+}
